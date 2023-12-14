4c2f0819560fdc737d36fd4db5c6cbaf71f5cf9c
Varun <varunrawat343@gmail.com>
2023-12-14 15:45:40 +0530

    [update]

--- FileHandling/toDo.py
+++ FileHandling/toDo.py
@@ -1,22 +1,29 @@
 f = open("salary.txt", "w")
 
-# f.writeline("10000")
-# f.writeline("12000")
 f.write("10000")
 f.write("\n")
 f.write("10003")
+f.write("\n")
+f.write("12000")
+f.write("\n")
+f.write("15003")
+f.write("\n")
+f.write("20000")
+f.write("\n")
+f.write("50003")
 
 f.close()
 
 
-# f = open("salary.txt", "r")
+f = open("salary.txt", "r")
 
-# sum = 0
-# while True:
-#     l = f.readline()
-#     if (l == ''):
-#         break
-#     sum += int(l)
-#     print(l)
+sum = 0
+while True:
+    l = f.readline()
+    if (l == ''):
+        break
+    sum += int(l)
+    print(l)
 
-# print(f"Sum is {sum}")
+print("\n")
+print(f"Sum is {sum}")
